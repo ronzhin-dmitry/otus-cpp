ec9cc37de5542adee7e2254d70303087680b485b
Dmitry Ronzhin <46577851+ronzhin-dmitry@users.noreply.github.com>
2024-11-06 11:33:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-//test update
+//test update-action
 int main(int, char **) {
 	std::cout << "Version: " << version() << std::endl;
 	std::cout << "Hello, world!" << std::endl;
