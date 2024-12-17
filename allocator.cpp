fb01eb3adfff2fbebd9e43d9aef1fb5a42c9b2aa
Dmitry Ronzhin <46577851+ronzhin-dmitry@users.noreply.github.com>
2024-12-17 22:48:28 +0300

    added dummy custom container + calls in main

--- allocator.cpp
+++ allocator.cpp
@@ -2,19 +2,54 @@
 
 int main()
 {
+    std::cout << "--------------------------------------------" << std::endl;
+    std::cout << "STD container + STD allocator" << std::endl;
+    //STD allocator for std::map
+    {
+        auto m_std = std::map<int, int>{};
+        for(int i = 0 ; i < 10; i++)
+            m_std[i] = iter_factorial(i);
+        for(int i = 0 ; i < 10; i++)
+            std::cout << "m_std[" << i << "] = " << m_std[i] << std::endl;
+    }
+
+    std::cout << "--------------------------------------------" << std::endl;
+    std::cout << "STD container + Custom allocator" << std::endl;
     //Custom allocator for std::map - max size 10
     {
-        auto m = std::map<int, int, std::less<int>, linear_allocator<std::pair<const int, int>, 10>>{};
+        auto m_custom = std::map<int, int, std::less<int>, linear_allocator<std::pair<const int, int>, 10>>{};
         for(int i = 0 ; i < 10; i++)
-            m[i] = iter_factorial(i);
+            m_custom[i] = iter_factorial(i);
         for(int i = 0 ; i < 10; i++)
-            std::cout << "m[" << i << "] = " << m[i] << std::endl;
-        
-        //m[10] = iter_factorial(10); // here we get abort, idk how to catch it
+            std::cout << "m_custom[" << i << "] = " << m_custom[i] << std::endl;
     }
+
+    std::cout << "--------------------------------------------" << std::endl;
+    std::cout << "Custom container + custom allocator" << std::endl;
+
+    //Custom container with custom allocator
+    {
+        auto v_custom = MyVec<int, linear_allocator<int, 10>>{};
+        v_custom.reserve(10);
+        //Custom allocator for std::map - max size 10
+        for(int i = 0 ; i < 10; i++)
+        {
+            v_custom.push_back(iter_factorial(i));
+        }
+        std::cout << "custom container with custom allocator elements:" << std::endl;
+        for(auto i = v_custom.begin() ; i != v_custom.end(); i++)
+            std::cout << (*i) << std::endl;
+        
+    }
+    //Bad range - throw error
+    try {
+    std::cout << "--------------------------------------------" << std::endl;
+    std::cout << "STD container + custom allocator + memory overflow" << std::endl;
     auto m = std::map<int, int, std::less<int>, linear_allocator<std::pair<const int, int>, 10>>{};
     for(int i = 0 ; i < 11; i++)
          m[i] = iter_factorial(i); //get alloc error, idk how to catch
-
+    } catch (const std::bad_alloc& e) {
+        std::cout << "Memory allocator bad alloc: " << e.what() << std::endl; //catched badalloc
+    }
     return 0;
 }
--- lib.h
+++ lib.h
@@ -51,25 +51,29 @@ struct linear_allocator {
         {
             std::cout << "real allocation here" << std::endl;
 		    data = (pointer)(std::malloc(MAX_SIZE * sizeof(T)));
+			std::cout << "allocated address " << data << std::endl;
         }
 		if (!data)
 			throw std::bad_alloc();
         el_count += n;
+		std::cout << "cur el count = " << el_count << std::endl;
 		return reinterpret_cast<T *>(data + el_count);
 	}
 
 	void deallocate(T *p, std::size_t n) {
+		std::cout << "before dealloc el_count = " << el_count << std::endl;
+		el_count -= n;
 #ifndef USE_PRETTY
-		std::cout << "deallocate: [n  = " << n << "], address " << p << std::endl;
+		std::cout << "deallocate: [n  = " << n << "], address " << p << " cur el count " << el_count << std::endl;
 #else
 		std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" << std::endl;
 #endif
-        el_count -= n;
+        
         if(data != nullptr && el_count == 0)
         {
+			std::cout << "Actual free address: " << data << std::endl;
 		    std::free(data);
             data = nullptr;
-            std::cout << "Actual free" << std::endl;
         }
 	}
 
@@ -95,3 +99,175 @@ struct linear_allocator {
 };
 
 unsigned int iter_factorial(unsigned int n);
+
+
+// Custom vector container
+template <class T, class Allocator = std::allocator<T>> class MyVec{
+  public:
+    // Type definitions for easier access to types used in the container
+    // Type of elements stored in the container
+    using value_type = T;
+
+    // Allocator type used for memory management
+    using allocator_type = Allocator;
+
+    // Reference type to the container's elements
+    using reference = value_type &;
+
+    // Constant reference type to the container's elements
+    using const_reference = const value_type &;
+
+    // Type for size and capacity of the container
+    using size_type = size_t;
+
+    // Type for difference between iterator positions
+    using difference_type = ptrdiff_t;
+
+    // Iterator class for the custom container
+    class iterator{
+      public:
+        // Type definitions for the iterator
+
+        // Category tag for random access iterators
+        using iterator_category = std::random_access_iterator_tag;
+
+        // Type of elements the iterator points to
+        using value_type = T;
+
+        // Type for difference between iterators
+        using difference_type = ptrdiff_t;
+
+        // Pointer type to the element
+        using pointer = T *;
+        // Reference type to the element
+        using reference = T &;
+
+        // Constructor to initialize the iterator with a pointer to an element
+        iterator(pointer ptr) : ptr_(ptr) {
+        }
+
+        // Dereference operator to access the element pointed to by the iterator
+        reference operator*() const{
+            return *ptr_;
+        }
+
+        // Arrow operator to access the element's members
+        pointer operator->() const{
+            return ptr_;
+        }
+
+        // Prefix increment operator to move the iterator to the next element
+        iterator &operator++(){
+            ++ptr_;
+            return *this;
+        }
+
+        // Postfix increment operator to move the iterator to the next element and return the previous
+        // position
+        iterator operator++(int) {
+            iterator temp = *this;
+            ++ptr_;
+            return temp;
+        }
+
+        // Equality operator to compare if two iterators are equal (point to the same element)
+        bool operator==(const iterator &other) const{
+            return ptr_ == other.ptr_;
+        }
+
+        // Inequality operator to compare if two iterators are not equal (point to different elements)
+        bool operator!=(const iterator &other) const{
+            return ptr_ != other.ptr_;
+        }
+
+      private:
+        pointer ptr_; // Pointer to the element the iterator currently points to
+    };
+
+    // Default constructor to create an empty container
+    MyVec() : data_(nullptr), size_(0), capacity_(0){
+		data_ = my_alloc.allocate(0); //wonderfully dumb solution
+    }
+
+	void reserve(size_t n)
+	{
+		//data_ = allocator_type().allocate(n);
+		std::cout << "Reserved data: " << data_ << std::endl;
+		capacity_ = n;
+	}
+
+
+    // Destructor to clean up the allocated memory when the container is destroyed
+    ~MyVec(){
+        // Deallocate the allocated memory
+		std::cout << "Call vector destructor of size " << size_ << " and capacity " << capacity_ << std::endl;
+		std::cout << "Cur data_: " << data_ << std::endl;
+        my_alloc.deallocate(data_, capacity_);
+    }
+
+    // Member function to add a new element at the end of the container
+    void push_back(const T &value){
+		T *cur_data = my_alloc.allocate(1);
+        cur_data[0] = value;
+    }
+
+    // Member function to remove the last element from the container
+    void pop_back(){
+        // Only remove if the container is not empty
+        if (size_ > 0){
+            --size_;
+        }
+    }
+
+    // Member function to access an element at a given index with bounds checking
+    reference at(size_type index){
+        if (index >= size_){
+
+            // Throw an exception if the index is invalid
+            throw std::out_of_range("Index out of range");
+        }
+        return data_[index];
+    }
+
+    // Const version of the 'at' function to access elements in a read-only context
+    const_reference at(size_type index) const{
+        if (index >= size_){
+
+            // Throw an exception if the index is invalid
+            throw std::out_of_range("Index out of range");
+        }
+        return data_[index];
+    }
+
+    // Member function to get the number of elements currently in the container
+    size_type size() const{
+        return size_;
+    }
+
+    // Member function to check if the container is empty
+    bool empty() const{
+        return size_ == 0;
+    }
+
+    // Member function to get an iterator pointing to the first element of the container
+    iterator begin(){
+        return iterator(data_);
+    }
+
+    // Member function to get an iterator pointing to the end (one past the last element) of the container
+    iterator end(){
+        return iterator(data_ + size_);
+    }
+
+  private:
+    // Pointer to the container's elements
+    T *data_;
+
+    // Number of elements in the container
+    size_type size_;
+
+    // Allocated capacity of the container
+    size_type capacity_;
+
+	allocator_type my_alloc;
+};
