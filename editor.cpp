8b71e98cf70ef18d2bb4e347d3df807d1e774c25
Dmitry Ronzhin <46577851+ronzhin-dmitry@users.noreply.github.com>
2025-01-03 15:32:28 +0300

    Added simple doxy

--- editor.cpp
+++ editor.cpp
@@ -1,5 +1,16 @@
 #include "lib.h"
 
+/**
+ * @brief Entry point
+ *
+ * Execution of the program
+ * starts here.
+ *
+ * @param argc Number of arguments
+ * @param argv List of arguments
+ *
+ * @return Program exit status
+ */
 int main()
 {
     //MVC self-made arch example
--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,10 @@
 #include "lib.h"
 #include "version.h"
 
+/**
+ * @brief function to produce release version in github workflows
+ * @return current release version (incremented on git push)
+ */
 int version()
 {
 	return PROJECT_VERSION_PATCH;
