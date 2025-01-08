c68e38eafb2b46fc1f7b732c80ada2259435e59a
Dmitry Ronzhin <d.v.ronjin@gmail.com>
2025-01-08 23:40:54 +0300

    Initial classes declaration (no body yet)

--- lib.h
+++ lib.h
@@ -9,8 +9,31 @@
 #include <tuple>
 #include <algorithm>
 #include <type_traits>
+#include <unordered_map>
 
 /**
  * @brief simple function to return version of the release
  */
 int version();
+
+/**
+ * @brief class to store sparse matrix
+ * no constructor will be publicly available
+ * only possible access point would be proxy-class Matrix
+ */
+template <typename T>
+class IMatrix
+{
+private:
+    std::unordered_map<std::pair<uint64_t,uint64_t>, T> index;
+public:
+
+};
+
+template <typename T, int D>
+class Matrix
+{
+private:
+    IMatrix<T> matrix;
+public:
+};
