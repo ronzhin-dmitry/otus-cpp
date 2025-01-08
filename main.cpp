6f7391f527690d0152d7f58f6982e102f051983b
Dmitry Ronzhin <46577851+ronzhin-dmitry@users.noreply.github.com>
2025-01-08 19:18:05 +0300

    empty project (no matrix implemented yet)

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "lib.h"
+
+
+/**
+ * @brief Entry point
+ *
+ * Execution of the program
+ * starts here.
+ *
+ * @param argc Number of arguments
+ * @param argv List of arguments
+ *
+ * @return Program exit status
+ */
+int main()
+{
+    //Sparse matrix made with Proxy-pattern -- usage
+    return 0;
+}
