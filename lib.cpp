9824351fea96c5a735e926372275ce799446c9df
Dmitry Ronzhin <d.v.ronjin@gmail.com>
2025-01-11 17:55:03 +0300

    Sparse 2D matrix with iteration + test + main + doxy

--- lib.cpp
+++ lib.cpp
@@ -1,3 +1,6 @@
+/**
+@file lib.cpp
+*/
 #include "lib.h"
 #include "version.h"
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
+/**
+@file main.cpp
+*/
 #include "lib.h"
-
 
 /**
  * @brief Entry point
@@ -14,6 +16,29 @@
  */
 int main()
 {
-    //Sparse matrix made with Proxy-pattern -- usage
+    //Usage of sparse matrix made with Proxy-pattern
+    Matrix<int, 0> mat;
+    for(int i = 0 ; i < 10 ; i++)
+    {
+        mat[i][i] = i;
+        mat[i][9-i] = 9-i;
+    }
+    for(int i = 1; i < 9; i++)
+    {
+        for(int j = 1; j < 9; j++)
+            std::cout << mat[i][j] << " ";
+        std::cout << std::endl;
+    }
+    std::cout << "Amount of empty cells = " << mat.size() << std::endl;
+    assert(mat.size() == 18);
+    std::cout << "Range based for iteration over non-zero cells:" << std::endl;
+    for(auto c: mat)
+    {
+        int x;
+        int y;
+        int v;
+        std::tie(x, y, v) = c;
+        std::cout << "mat[" << x << "][" << y << "] = " << v << std::endl;
+    }
     return 0;
 }
