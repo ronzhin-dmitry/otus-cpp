4c4baf1fced0ab3ae8a214a37691d6d6b3c1e355
Dmitry Ronzhin <d.v.ronjin@gmail.com>
2025-01-25 18:46:59 +0300

    Some init interfaces (not finished yet)

--- lib.h
+++ lib.h
@@ -2,167 +2,85 @@
 #include <cassert>
 #include <cstdlib>
 #include <iostream>
-#include <string>
-#include <vector>
 #include <list>
-#include <array>
-#include <tuple>
-#include <algorithm>
-#include <type_traits>
-#include <unordered_map>
+#include <string>
+#include <memory>
 
 /**
- * @brief simple function to return version of the release
+ * @brief simple function to return version of the release.
  */
 int version();
 
 /**
- * @brief class to store sparse matrix
- * contains class for proxy access (to support default value)
- * also has classes for iterator and a single row-slice (to access via [][])
+ * @brief general command class, self-contained. Allows serizalization and execution (possible) of a command.
  */
-template <typename T, int D>
-class Matrix
+class ICommand
 {
-protected:
-    std::unordered_map<uint64_t, std::unordered_map<uint64_t, T>> index;
-public:
-    /**
-     * @brief proxy-class for matrix to support default-value on empty cells
-     */
-    class MatrixProxy
-    {
-        Matrix *mat;
-        int i, j;
     public:
-        MatrixProxy(Matrix* m, int i_, int j_):mat(m), i(i_), j(j_) {};
-        operator T() const { 
-            if(mat->index.find(i) != mat->index.end() && mat->index[i].find(j) != mat->index[i].end())
-                return mat->index[i][j];
-            else
-                return T(D); 
-        };
+        virtual std::string serialize() = 0;
+        virtual void exec() {};
+};
 
-        void operator= (T value) {
-            if(value != D)
-                mat->index[i][j] = value;
-            else
-            {
-                if(mat->index.find(i) != mat->index.end() && mat->index[i].find(j) != mat->index[i].end())
-                    mat->index[i].erase(mat->index[i].find(j));
-            }
-        };
-    };
-    /**
-     * @brief helper class to represent matrix row (and allow indexing via mat[][])
-     */
-    class MatrixRow
-    {
+class DumbCommand:ICommand
+{
     private:
-        Matrix *parent;
-        int row_num;
+        std::string body;
+        DumbCommand(){};
     public:
-        MatrixRow(Matrix* par, int R): parent(par), row_num(R){};
-        MatrixProxy operator[](int col_num) {
-            return MatrixProxy{parent, row_num, col_num};
-        };
-        const MatrixProxy operator[](int col_num) const {
-            return MatrixProxy{parent, row_num,col_num};
-        };
-    };
-
-    /**
-     * @brief returns matrix row class which allows for double brackets
-     */
-    MatrixRow operator[](int row_num) {return MatrixRow(this, row_num);};
-     /**
-     * @brief returns matrix row class which allows for double brackets
-     */
-    const MatrixRow operator[](int row_num) const {return MatrixRow(this, row_num);};
-     
-     /**
-     * @brief just some helper function to print container (without iterators of matrix)
-     */
-    void print_container()
-    {
-        for(auto it1 = index.begin(); it1 != index.end() ; it1++)
+        std::string serialize() override
         {
-            for(auto it2 = it1->second.begin(); it2 != it1->second.end(); it2++)
-            {
-                std::cout  << "Matrix[" << it1->first << "][" << it2->first << "] = " << it2->second << std::endl;
-            }
+            return body;
         }
-    }
-    Matrix(){};
 
-    /**
-     * @brief helper function to retrieve container size (iterates over inner container)
-     * Currently not optimal, probably can be done better via some internatl counters for non-default elements
-     */
-    size_t size()
-    {
-        if(index.size() == 0)
-            return 0;
-        else
-        {
-            size_t res = 0;
-            for(auto r: index)
-                res += r.second.size();
-            return res;
-        }
-    }
-    /**
-     * @brief iterator class for sparse matrix
-     * has to deal with double-indexed container inside
-     */
-    class ItemIterator {
-        typename std::unordered_map<uint64_t, std::unordered_map<uint64_t, T>>::iterator i_outer;
-        typename std::unordered_map<uint64_t, T>::iterator i_inner;
-        Matrix <T,D> &mat;
+        DumbCommand(std::string cmd):body(cmd){};
+};
+
+//TODO - add observer logger class
+
+class Application;
+
+/**
+ * @brief Virtual class for current state of parser (we model it with finite automaton)
+ * Each state implemented via singletone pattern
+ */
+class IState
+{
+public:
+    virtual IState& getInstance() = 0;
+    virtual void processInput(Application *) = 0;
+};
+
+/**
+ * @brief Base class for context of the automaton, allows for context switching based on states and inputs
+ */
+class Application
+{
+    private:
+    IState* curState;
+    Application(){};
     public:
-        ItemIterator(Matrix<T,D> &mat_, typename std::unordered_map<uint64_t, std::unordered_map<uint64_t, T>>::iterator i): i_outer(i), mat(mat_) {
-            if(i_outer  != mat.index.end())
-                i_inner = i_outer->second.begin();
+    void setCurrentState(IState* newState) {
+            curState = newState;
+         }
+
+    Application(IState* initialState)
+    {
+        curState = initialState;
+    }
+
+    void runApp()
+    {
+        std::cout << "app started" << std::endl;
+        while(curState != nullptr)
+        {
+            curState->processInput(this);
         }
+        std::cout << "app finished" << std::endl;
+    }
 
-        // incrementing
-        ItemIterator & operator ++() { 
-            if(i_outer  != mat.index.end() && i_inner != i_outer->second.end())
-                ++i_inner; 
-            if(i_inner ==  i_outer->second.end())
-            {
-                ++i_outer;
-                if(i_outer  != mat.index.end())
-                {
-                    i_inner = i_outer->second.begin();
-                }
-            }
-            return *this; 
-        }
-        ItemIterator operator ++(int) { 
-            auto old(*this);
-            ++(*this);
-            return old; }
-
-        // comparison
-        bool operator!=(const ItemIterator &o) const { 
-            if(i_outer  != mat.index.end())
-                return (i_outer != o.i_outer) || (i_inner != o.i_inner); 
-            else
-                return i_outer != o.i_outer;
-        }
-
-        // dereferencing
-        const auto operator*() const { 
-            return std::make_tuple(i_outer->first, i_inner->first, i_inner->second); 
-        }
-        friend class Matrix;
-    };
-
-    ItemIterator begin() { return ItemIterator(*this, index.begin()); }
-    ItemIterator end()   { return ItemIterator(*this, index.end()  ); }
-
-    friend class MatrixRow;
-    friend class MatrixProxy;
-    friend class ItemIterator;
+    void terminate()
+    {
+        curState = nullptr;
+        //TODO - consider additional logic
+    }
 };
--- main.cpp
+++ main.cpp
@@ -16,29 +16,8 @@
  */
 int main()
 {
-    //Usage of sparse matrix made with Proxy-pattern
-    Matrix<int, 0> mat;
-    for(int i = 0 ; i < 10 ; i++)
-    {
-        mat[i][i] = i;
-        mat[i][9-i] = 9-i;
-    }
-    for(int i = 1; i < 9; i++)
-    {
-        for(int j = 1; j < 9; j++)
-            std::cout << mat[i][j] << " ";
-        std::cout << std::endl;
-    }
-    std::cout << "Amount of empty cells = " << mat.size() << std::endl;
-    assert(mat.size() == 18);
-    std::cout << "Range based for iteration over non-zero cells:" << std::endl;
-    for(auto c: mat)
-    {
-        int x;
-        int y;
-        int v;
-        std::tie(x, y, v) = c;
-        std::cout << "mat[" << x << "][" << y << "] = " << v << std::endl;
-    }
+    //Command parser - entry points. Main only calls for app init.
+    Application app(nullptr); //TODO - put valid state
+    app.runApp();
     return 0;
 }
