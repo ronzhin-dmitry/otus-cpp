c4a69590e184fe4dfaad8fad80f5a17a65f14527
Dmitry Ronzhin <46577851+ronzhin-dmitry@users.noreply.github.com>
2025-04-21 21:19:41 +0300

    Added some boost tests

--- test_version.cpp
+++ test_version.cpp
@@ -7,6 +7,15 @@
 #include <sstream>
 
 #include <boost/test/unit_test.hpp>
+#include <cmath>
+
+#include <boost/asio.hpp>
+#include <thread>
+#include <future>
+
+using namespace boost::asio;
+using namespace boost::asio::ip;
+
 
 BOOST_AUTO_TEST_SUITE(test_version)
 
@@ -14,4 +23,130 @@ BOOST_AUTO_TEST_CASE(test_valid_version)
 {
 	BOOST_CHECK(version() > 0);
 }
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(test_expression_parser)
+
+BOOST_AUTO_TEST_CASE(test_simple_expression) {
+    expression_parser::ExpressionParser ep("3 + 4 * 2");
+    BOOST_CHECK_CLOSE(ep.evaluate(0), 11.0, 1e-6);
+}
+
+BOOST_AUTO_TEST_CASE(test_variable_and_constants) {
+    expression_parser::ExpressionParser ep("x + pi - e");
+    double x_val = 1.0;
+    double expected = x_val + M_PI - M_E;
+    BOOST_CHECK_CLOSE(ep.evaluate(x_val), expected, 1e-6);
+}
+
+BOOST_AUTO_TEST_CASE(test_function_evaluation) {
+    expression_parser::ExpressionParser ep("sin(pi/2) + log(e, e^3)");
+    BOOST_CHECK_CLOSE(ep.evaluate(0), 1.0 + 3.0, 1e-6);
+}
+
+BOOST_AUTO_TEST_CASE(test_complex_expression) {
+    expression_parser::ExpressionParser ep("2*(3 + x^2) - ln(e^5)");
+    double x_val = 2.0;
+    double expected = 2*(3 + pow(x_val, 2)) - 5;
+    BOOST_CHECK_CLOSE(ep.evaluate(x_val), expected, 1e-6);
+}
+
+BOOST_AUTO_TEST_CASE(test_invalid_expression) {
+    // Некорректная функция: исключение при создании парсера
+    BOOST_CHECK_THROW(expression_parser::ExpressionParser ep("invalid_func(x)"), std::invalid_argument);
+    
+    // Синтаксическая ошибка: исключение при вычислении
+    expression_parser::ExpressionParser ep("3 + * x");
+    BOOST_CHECK_THROW(ep.evaluate(0), std::invalid_argument);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE(test_integrator)
+
+BOOST_AUTO_TEST_CASE(test_sin_integration) {
+    monte_carlo_multithread::Integrator integrator;
+    auto result_str = integrator.execute([](double x) { return std::sin(x); }, 0.0, M_PI, 1'000'000, 4);
+    double result = std::stod(result_str);
+    BOOST_CHECK_CLOSE(result, 2.0, 1.0); // 1% tolerance
+}
+
+BOOST_AUTO_TEST_CASE(test_square_integration) {
+    monte_carlo_multithread::Integrator integrator;
+    auto result_str = integrator.execute([](double x) { return x*x; }, 0.0, 1.0, 1'000'000, 4);
+    double result = std::stod(result_str);
+    BOOST_CHECK_CLOSE(result, 1.0/3.0, 2.0); // 2% tolerance
+}
+
+BOOST_AUTO_TEST_CASE(test_variance_based_calculation) {
+    // Увеличиваем количество точек и допуск
+    std::string query = "1;x^2;0;1;0.0001;4"; // Более строгая дисперсия
+    monte_carlo_multithread::Integrator integrator;
+    std::string result_str = integrator.execute(query);
+    double result = std::stod(result_str);
+    BOOST_CHECK_CLOSE(result, 1.0/3.0, 5.0); // 5% допуск
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(test_server)
+
+std::string execute_server_test(const std::string& query, short port) {
+    io_service io_context;
+    tcp::socket socket(io_context);
+    
+    std::promise<void> connected;
+    auto future = connected.get_future();
+    
+    std::thread client_thread([&]{
+        socket.connect(tcp::endpoint(address::from_string("127.0.0.1"), port));
+        connected.set_value();
+        
+        write(socket, buffer(query + "\n"));
+        
+        streambuf response_buf;
+        read_until(socket, response_buf, '\n');
+        std::istream response_stream(&response_buf);
+        std::string response;
+        std::getline(response_stream, response);
+        return response;
+    });
+
+    future.wait();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Wait for server
+    
+    // Cleanup
+    client_thread.join();
+    return "";
+}
+
+BOOST_AUTO_TEST_CASE(test_server_connection_and_response) {
+    const short test_port = 6666;
+    
+    io_service io_context;
+    monte_carlo_server::Server server(io_context, test_port);
+    
+    std::thread server_thread([&io_context]{
+        io_context.run();
+    });
+    
+    // Test client
+    tcp::socket socket(io_context);
+    socket.connect(tcp::endpoint(address::from_string("127.0.0.1"), test_port));
+    
+    std::string query = "0;x^2;0;1;1000000;4";
+    write(socket, buffer(query + "\n"));
+    
+    streambuf response_buf;
+    read_until(socket, response_buf, '\n');
+    std::string response = buffer_cast<const char*>(response_buf.data());
+    
+    double result = std::stod(response);
+    BOOST_CHECK_CLOSE(result, 0.333, 5.0);
+    
+    io_context.stop();
+    server_thread.join();
+}
+
 BOOST_AUTO_TEST_SUITE_END()
